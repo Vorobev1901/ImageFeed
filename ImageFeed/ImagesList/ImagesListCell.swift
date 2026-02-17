6e785a627573de440be7da2e30c8742f7a4aba22
Nikita <nik.vorobev.1901@yandex.ru>
2026-02-17 12:40:31 +0300

    feat: implement main screen layout with table view

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,20 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Nikita Vorobiev on 14.02.2026.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    
+    var likeState: LikeState = .off
+    
+    
+}
